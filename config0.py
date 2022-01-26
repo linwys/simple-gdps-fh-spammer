b32e8e4079a649422b5c1b02ab62ce5d7edcc9f1
Aluminum <91447283+AluminumFF@users.noreply.github.com>
2022-01-26 23:28:36 +0300

    Add files via upload

---
+++ config0.py
@@ -0,0 +1,8 @@
+name = 'Mystery self stresser'
+stars = '2147483647'
+demons = '-8'
+diamonds = '-88'
+userCoins = '-888'
+
+#должно заканчиватся на /
+dataBase = 'http://ps.fhgdps.com/Tal99887gdps/'
