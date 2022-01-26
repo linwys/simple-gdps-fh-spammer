322641f96a039250c4e01920eaf0e7365f582b2b
Aluminum <91447283+AluminumFF@users.noreply.github.com>
2022-01-26 23:29:32 +0300

    Update config0.py

--- config0.py
+++ config0.py
@@ -1,8 +1,8 @@
-name = 'Mystery self stresser'
+name = 'Dezzalir lox'
 stars = '2147483647'
 demons = '-8'
 diamonds = '-88'
 userCoins = '-888'
 
 #должно заканчиватся на /
-dataBase = 'http://ps.fhgdps.com/Tal99887gdps/'
+dataBase = 'http://ps.fhgdps.com/CUSTOM URL OF GDPS/'
